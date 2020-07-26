c4494b5d7c8d8d8dc21ff92cb081884c806770cd
Albert Son <juliuscaesarson@ip-172-31-48-57.ec2.internal>
2020-07-25 20:13:33 -0500

    geturl

--- soundcloud/src/home.js
+++ soundcloud/src/home.js
@@ -20,30 +20,48 @@ class Home extends Component {
 
   onChangeFile(file) {
     this.setState({
-      file: file[0]
+      file
     })
+    console.log(file[0].name);
   }
 
   upload() {
-    const file = this.state.file
-    const storageRef = fire.storage().ref();
-    const fileRef = storageRef.child(file.name);
-    fileRef.put(file).then(() => {
-      console.log("Uploaded file");
-    })
+    if (this.state.file != null) {
+      const file = this.state.file[0]
+      const storageRef = fire.storage().ref();
+      // Checks to see if file name already exists in user's folder
+      // Because Firebase API can only request a file that exists, checking for a non-existent file is handled as an error
+      // This is why I needed to use the Promise error as a condition for inserting music files
+      storageRef.child(fire.auth().currentUser.uid).child(file.name).getDownloadURL().then(onResolve, onReject);
+      function onResolve(foundURL) {
+        console.log("Filename already exists");
+      }
+      function onReject(error) {
+        // Creates a folder within the database if current user's own folder doesn't exist, but if it already exists, it will put the uploaded file into user specific directory
+        const fileRef = storageRef.child(fire.auth().currentUser.uid).child(file.name);
+        fileRef.put(file).then(() => {
+          console.log("Uploaded file: " + file.name);
+      })
+    }
+    }
+    else {
+      alert("Please select a file to upload");
+    }
     
   }
-
+  
 
   render() {
     return (
-        <div>
-            <h1>Home</h1>
-            <input type="file" onChange={(e) => {this.onChangeFile(e.target.files)}} />
-            <button onClick={this.upload}>Upload</button>
-            <button onClick={this.logout} className="btn btn-success">Logout</button>
-            {this.state.email}
+      <div className="container">
+        <div className="page-header">
+          <h1>React Player</h1>
         </div>
+          <input type="file" onChange={(e) => {this.onChangeFile(e.target.files)}} />
+          <button onClick={this.upload}>Upload</button>
+          <button onClick={this.logout} className="btn btn-success">Logout</button>
+          
+      </div>
     );
   }
 }
